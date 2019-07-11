e7c1b3ffe4c745d0dca7f19ba92263d369b13cd2
Matthew Rothery <matthewrothery@outlook.com>
2019-07-11 11:32:24 +1000

    revert changes

--- src/index.js
+++ src/index.js
@@ -16,14 +16,6 @@ class ReactStripeModal extends React.Component {
         this.close = this.close.bind(this);
     }
 
-    componentDidUpdate(prevProps) {
-        if (this.state.open !== this.props.open && prevProps.open !== this.props.open) {
-            this.setState({
-                open: this.props.open
-            });
-        }
-    }
-
     // Open the modal
     open() {
         this.setState({ open: true });
